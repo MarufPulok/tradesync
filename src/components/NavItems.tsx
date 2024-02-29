b45a048534fc34ccf0db175f2729e4b95c45e03e
MarufPulok <maruf12@student.sust.edu>
2024-02-29 18:12:16 +0600

    assets && navbar initial UI

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { PRODUCT_CATEGORIES } from "@/config";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+
+export default function NavItems() {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+  
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        const isOpen = index === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+}
